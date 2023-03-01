a522ee63320b0a17d5889d995e586009d47fc9d5
alexktvsky <alexktvsky@gmail.com>
2023-03-01 22:22:50 +0600

    Add feature to display a pyramid representation of the loaded image

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -24,6 +24,16 @@ MainWindow::MainWindow(QWidget *parent)
         this,
         &MainWindow::onActionOpenImage
     );
+
+    m_layer_combobox = new QComboBox(this);
+    m_layer_combobox->setEnabled(false);
+    connect(
+        m_layer_combobox,
+        qOverload<int>(&QComboBox::currentIndexChanged),
+        this,
+        &MainWindow::onActionselectLayer
+    );
+    statusBar()->addPermanentWidget(m_layer_combobox);
 }
 
 void MainWindow::onActionOpenImage(void)
@@ -39,7 +49,75 @@ void MainWindow::onActionOpenImage(void)
         return;
     }
 
-    QPixmap pixmap(fname);
-    m_label->setPixmap(pixmap);
-    m_label->resize(pixmap.size());
+    QPixmap orig_pixmap(fname);
+    m_label->setPixmap(orig_pixmap);
+    m_label->resize(orig_pixmap.size());
+
+    initializeImagePyramid(orig_pixmap);
+    initializeComboBox();
+}
+
+void MainWindow::initializeImagePyramid(const QPixmap &pixmap)
+{
+    m_image_pyramid.clear();
+    m_image_pyramid.push_back(pixmap);
+
+    while (m_image_pyramid.back().width() > 1
+        && m_image_pyramid.back().height() > 1)
+    {
+        QPixmap scaled_pixmap = m_image_pyramid.back().scaled(
+            m_image_pyramid.back().width() / 2,
+            m_image_pyramid.back().height() / 2
+        );
+        m_image_pyramid.push_back(scaled_pixmap);
+    }
+}
+
+void MainWindow::initializeComboBox(void)
+{
+    m_layer_combobox->setEnabled(true);
+    m_layer_combobox->clear();
+
+    QString orig_image_name = tr("Original, Size: %1 x %2")
+        .arg(m_image_pyramid.front().width())
+        .arg(m_image_pyramid.front().height());
+
+    m_layer_combobox->addItem(orig_image_name);
+
+    for (size_t i = 1; i < m_image_pyramid.size(); ++i) {
+        size_t width = m_image_pyramid[i].width();
+        size_t height = m_image_pyramid[i].height();
+        QString layer_name = tr("Layer: %1, Size: %2 x %3")
+            .arg(i)
+            .arg(width)
+            .arg(height);
+        m_layer_combobox->addItem(layer_name);
+    }
+}
+
+void MainWindow::onActionselectLayer(int index)
+{
+    if (static_cast<size_t>(index) >= m_image_pyramid.size()) {
+        return;
+    }
+
+    QPixmap layer = m_image_pyramid[index];
+
+    if (index == 0) {
+        m_label->setPixmap(layer);
+    }
+    else {
+
+        int width = m_image_pyramid.front().width();
+        int height = m_image_pyramid.front().height();
+
+        QPixmap scaled_layer = layer.scaled(
+            width,
+            height,
+            Qt::KeepAspectRatio,
+            Qt::SmoothTransformation
+        );
+
+        m_label->setPixmap(scaled_layer);
+    }
 }
--- src/main_window.h
+++ src/main_window.h
@@ -1,6 +1,7 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include <vector>
 #include <QtWidgets>
 
 
@@ -11,11 +12,17 @@ public:
 
 private slots:
     void onActionOpenImage(void);
+    void onActionselectLayer(int index);
+
+private:
+    void initializeImagePyramid(const QPixmap &pixmap);
+    void initializeComboBox(void);
 
 private:
     QLabel *m_label;
     QScrollArea *m_scroll_area;
-
+    std::vector<QPixmap> m_image_pyramid;
+    QComboBox *m_layer_combobox;
 }; // MainWindow
 
 
