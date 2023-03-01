830cb6eb00337d091fe39801b96a42c07e8709dc
alexktvsky <alexktvsky@gmail.com>
2023-03-01 19:49:31 +0600

    Add feature to load PNG and JPG images and display their content

--- src/main_window.cpp
+++ src/main_window.cpp
@@ -3,11 +3,41 @@
 
 MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent) {
 
-    QPixmap pixmap(500, 500);
-    pixmap.fill(Qt::white);
+    m_label = new QLabel(this);
+    m_label->setAlignment(Qt::AlignCenter);
 
-    QLabel *label = new QLabel(this);
-    label->setPixmap(pixmap);
-    label->setAlignment(Qt::AlignCenter);
-    setCentralWidget(label);
+    m_scroll_area = new QScrollArea(this);
+    m_scroll_area->setWidgetResizable(true);
+    m_scroll_area->setWidget(m_label);
+    setCentralWidget(m_scroll_area);
+
+    QMenuBar *menu_bar = new QMenuBar(this);
+    setMenuBar(menu_bar);
+    QMenu *file_menu = menu_bar->addMenu(tr("&File"));
+    QAction *open_action = new QAction(tr("&Open Image"), this);
+    open_action->setShortcut(QKeySequence("CTRL+O"));
+    file_menu->addAction(open_action);
+    connect(
+        open_action,
+        &QAction::triggered,
+        this,
+        &MainWindow::onActionOpenImage
+    );
+}
+
+void MainWindow::onActionOpenImage(void) {
+    QString fname = QFileDialog::getOpenFileName(
+        this,
+        tr("Open image..."),
+        QDir::currentPath(),
+        "*.png *.jpg"
+    );
+
+    if (fname.isEmpty()) {
+        return;
+    }
+
+    QPixmap pixmap(fname);
+    m_label->setPixmap(pixmap);
+    m_label->resize(pixmap.size());
 }
--- src/main_window.h
+++ src/main_window.h
@@ -9,6 +9,13 @@ Q_OBJECT
 public:
     MainWindow(QWidget *parent = nullptr);
 
+private slots:
+    void onActionOpenImage(void);
+
+private:
+    QLabel *m_label;
+    QScrollArea *m_scroll_area;
+
 }; // MainWindow
 
 
